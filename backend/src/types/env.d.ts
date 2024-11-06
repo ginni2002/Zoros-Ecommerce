32afd0c3b9b11edaf0c11b1c477c2911b85bd936
Giridhari <grsb2002@gmail.com>
2024-11-06 11:44:58 +0530

    updated env.d.ts file by adding more env types

--- backend/src/types/env.d.ts
+++ backend/src/types/env.d.ts
@@ -2,5 +2,13 @@ declare namespace NodeJS {
   interface ProcessEnv {
     PORT: string;
     MONGO_URI: string;
+    STRIPE_SECRET_KEY: string;
+    STRIPE_PUBLIC_KEY: string;
+    STRIPE_WEBHOOK_SECRET: string;
+    EMAIL_USER: string;
+    EMAIL_APP_PASSWORD: string;
+    JWT_SECRET: string;
+    JWT_EXPIRE: string;
+    FRONTEND_URL: string;
   }
 }
