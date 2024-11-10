7ae1c738d1904683b00832d9950e3c8b009a221b
Giridhari <grsb2002@gmail.com>
2024-11-10 15:16:30 +0530

    added security headers middleware

---
+++ backend/src/middleware/securityHeaders.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from "express";
+import helmet from "helmet";
+
+export const securityHeaders = [
+  helmet(),
+
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "'unsafe-inline'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+      imgSrc: ["'self'", "data:", "https:", "http:"],
+      connectSrc: ["'self'", "https://api.stripe.com"],
+      frameSrc: ["'self'", "https://js.stripe.com"],
+      objectSrc: ["'none'"],
+      upgradeInsecureRequests: [],
+    },
+  }),
+
+  (req: Request, res: Response, next: NextFunction) => {
+    res.setHeader(
+      "Cache-Control",
+      "no-store, no-cache, must-revalidate, proxy-revalidate"
+    );
+    res.setHeader("Pragma", "no-cache");
+    res.setHeader("Expires", "0");
+    res.setHeader("Surrogate-Control", "no-store");
+
+    res.setHeader("X-Download-Options", "noopen");
+    res.setHeader("X-Permitted-Cross-Domain-Policies", "none");
+
+    next();
+  },
+];
