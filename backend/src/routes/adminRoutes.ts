a8eaf5d47bcd3e572b09dd30bb9daa9bb4ee9e2a
Giridhari <grsb2002@gmail.com>
2024-11-10 15:17:56 +0530

    updated adminRoutes.ts

--- backend/src/routes/adminRoutes.ts
+++ backend/src/routes/adminRoutes.ts
@@ -1,8 +1,27 @@
 import express, { Router } from "express";
-import { addMockData } from "../controllers/adminController";
+import {
+  addMockData,
+  clearRateLimits,
+  checkRateLimits,
+} from "../controllers/adminController";
+// import { validateFileUpload } from "../middleware/requestValidator";
+// import { ALLOWED_IMAGE_TYPES, FILE_SIZE_LIMITS } from "../types/file.types";
 
 const router: Router = express.Router();
 
 router.post("/mock/add_Data", addMockData);
 
+router.post("/clear-rate-limits", clearRateLimits);
+router.get("/check-limits", checkRateLimits);
+
+// router.post(
+//   "/mock-add_Data-with_Image",
+//   validateFileUpload({
+//     allowedTypes: ALLOWED_IMAGE_TYPES,
+//     maxSize: FILE_SIZE_LIMITS.IMAGE,
+//     required: true,
+//     maxFiles: 5,
+//   }),
+//   addMockDataWithImage
+// );
 export default router;
